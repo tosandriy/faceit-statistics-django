fb5600f2c56e8784632f4f3ca736363190dae8c0
GuK1337 <sb05032002@gmail.com>
2020-09-14 19:43:13 +0400

    Podarok dlya Antona

--- faceit_stats_django/parser.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-print(requests.get("https://open.faceit.com/data/v4/players?nickname=TOSANDRIY",
-                             headers={ "accept": "application/json", "Authorization": "Bearer 07975108-f7ed-46dd-9e58-962350c09afd"}).text)
